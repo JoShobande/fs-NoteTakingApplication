816f4fcbae9a40101213dc8a82bfb28a1f57e988
Josephine <shobande.josephin@gmail.com>
2025-04-10 13:51:24 -0400

    implementing menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,28 @@
+
+
+const Menu = () => {
+    return(
+        <section>
+            <div className='relative'>
+                <div className='border border-[red] bg-[white]'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est culpa consequuntur, hic debitis sunt pariatur. Quam, iusto deleniti voluptatem perspiciatis architecto tempora ex repellendus corrupti odio! Vero obcaecati repellat officiis, nobis nulla possimus maxime voluptate doloremque accusantium veniam rerum magnam iusto, labore debitis deleniti quidem doloribus error modi neque nihil sed at cumque delectus maiores! Nulla sed non debitis fuga dolores incidunt omnis sint similique itaque sunt placeat eos laboriosam odit aliquid perspiciatis hic, quia deleniti illo quas eveniet! Tenetur deserunt illo quos, repellendus itaque optio sapiente quibusdam laudantium praesentium nobis, dolores nostrum eligendi possimus ullam eveniet sint atque accusantium.
+                </div>
+                <div className='border w-[15%] h-screen bg-[white]'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt adipisci eveniet nulla exercitationem voluptas repellat provident eum velit quo possimus fugiat fuga esse nemo quos, blanditiis, porro nisi odio. Aperiam, repudiandae! Explicabo magni sed similique excepturi reiciendis voluptatum cupiditate voluptatibus culpa nemo vitae rem porro animi tempore, eum earum non, exercitationem velit. Veritatis porro expedita ducimus iste hic ullam eum.
+                </div>
+                <div className='absolute top-[15%] left-[15%] border '>
+                    <p>hello from menu</p>
+                    <p>hello from menu</p>
+                    <p>hello from menu</p>
+                    <p>hello from menu</p>
+                    <p>hello from menu</p>
+                    <p>hello from menu</p>
+                    <p>hello from menu</p>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
+
+export default Menu;
