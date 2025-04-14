52a8d8883aab92662317826259066f2bb16851f9
Josephine <shobande.josephin@gmail.com>
2025-04-14 14:33:16 -0400

    implemented layout structure deskop view

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,23 +1,46 @@
+import Image from "next/image";
 
 
 const Menu = () => {
     return(
         <section>
-            <div className='relative'>
-                <div className='border border-[red] bg-[white]'>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est culpa consequuntur, hic debitis sunt pariatur. Quam, iusto deleniti voluptatem perspiciatis architecto tempora ex repellendus corrupti odio! Vero obcaecati repellat officiis, nobis nulla possimus maxime voluptate doloremque accusantium veniam rerum magnam iusto, labore debitis deleniti quidem doloribus error modi neque nihil sed at cumque delectus maiores! Nulla sed non debitis fuga dolores incidunt omnis sint similique itaque sunt placeat eos laboriosam odit aliquid perspiciatis hic, quia deleniti illo quas eveniet! Tenetur deserunt illo quos, repellendus itaque optio sapiente quibusdam laudantium praesentium nobis, dolores nostrum eligendi possimus ullam eveniet sint atque accusantium.
-                </div>
-                <div className='border w-[15%] h-screen bg-[white]'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt adipisci eveniet nulla exercitationem voluptas repellat provident eum velit quo possimus fugiat fuga esse nemo quos, blanditiis, porro nisi odio. Aperiam, repudiandae! Explicabo magni sed similique excepturi reiciendis voluptatum cupiditate voluptatibus culpa nemo vitae rem porro animi tempore, eum earum non, exercitationem velit. Veritatis porro expedita ducimus iste hic ullam eum.
-                </div>
-                <div className='absolute top-[15%] left-[15%] border '>
-                    <p>hello from menu</p>
-                    <p>hello from menu</p>
-                    <p>hello from menu</p>
-                    <p>hello from menu</p>
-                    <p>hello from menu</p>
-                    <p>hello from menu</p>
-                    <p>hello from menu</p>
+            <div className='min-h-screen bg-gray-100'>
+                <header className="h-[100px] bg-white flex items-center py-[20px] ">
+                    <Image
+                        src='/logoNoText.png'
+                        alt='logo'
+                        height={200}
+                        width={200}
+                        className='pt-[30px]'
+                    />
+                    <div className='px-[80px]'>
+                        <h1 >MY NOTES</h1>
+                    </div>
+                </header>
+                <div className='flex'>
+                    <aside className="w-64 bg-white h-screen">
+                        <nav className="pl-[60px] pt-[40px]">
+                            <ul className="space-y-4">
+                                <li>Add New</li>
+                                <li>Calendar</li>
+                                <li>Archive</li>
+                                <li>Trash</li>
+                            </ul>
+                        </nav>
+                    </aside>
+                    <div className="flex-1 -ml-4 -mt-2 z-10">
+                        <div className="rounded-tl-[30px] overflow-hidden">
+                            <main className="px-8 py-6 bg-gray-200">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, at quia. 
+                                    Iusto nisi vero cumque id animi voluptatem iste perferendis est...
+                                </p>
+                                <p>Additional content …</p>
+                                <p>More content …</p>
+                                <p>Even more content …</p>
+                            </main>
+                        </div>
+                    </div>   
                 </div>
             </div>
             
