b0e0ad570480e8295bc51399335a22a63e2fcb62
Josephine <shobande.josephin@gmail.com>
2025-04-17 13:50:26 -0400

    mobile menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -47,16 +47,19 @@ const Menu = ({currentPage}:{currentPage?:ReactNode}) => {
                 <div className='block lg:flex'>
                     <aside 
                         className={`
-                            ${
-                                openMobileMenu 
-                                ? 
-                                    'block right-[0] static  top-0 w-[250px] h-auto' 
-                                :
-                                    'hidden'
-                            } 
-                            lg:block w-64 bg-white h-screen
+                            fixed inset-y-0 right-0 bg-white h-screen z-50 lg:z-0
+                            w-[250px]                      
+                            transition-transform duration-300 ease-in-out
+                            ${openMobileMenu ? "translate-x-0" : "translate-x-full"}
+                            lg:relative lg:translate-x-0 lg:inset-auto lg:w-64 lg:block
                         `}
                     >
+
+                        <div className="flex justify-end p-4 lg:hidden cursor-pointer">
+                            <button onClick={() => setOpenMobileMenu(false)} className='text-[30px]'>
+                                ✕
+                            </button>
+                        </div>
                         <nav className="pl-[60px] pt-[40px]">
                             <ul className="space-y-4">
                                 <li>Add New</li>
